8bd12bf0dbe4c03340572071a658a9617e6363c2
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-04 15:25:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  dd = []
+  attendees.each do |namos|
+   dd.push(badge_maker(namos))
+  end
+  dd
+end
+
+def assign_rooms(speakers)
+  jj = []
+  counter = 0 
+  speakers.each do |person|
+    counter += 1
+    jj.push("Hello, #{person}! You'll be assigned to room #{counter}!")
+  end
+  jj
+end
